417f150c5126d2cc4a5b36011b6f4c76454648d0
Aurélien Said Housseini <orel@MacBook-Air-de-Aurelien.local>
2023-03-06 11:50:36 +0100

    suppression d'un script utilisé avec switchboard

--- scripts/cc_finder.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-with open("convDict.pkl","rb") as f:
-    convDict = pickle.load(f)
-
-def get_utt_str(utterance):
-    return " ".join([token["text"] for token in utterance])
-speaker_test = convD
